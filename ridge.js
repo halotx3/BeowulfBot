898f683f577790bb0cc09859348d6df4e41f4885
Christopher Brownlee <cdbreach@gmail.com>
2019-11-28 13:49:35 -0500

    Addition of additional files base functionality, Currently working on input and storing of values next

---
+++ ridge.js
@@ -0,0 +1,20 @@
+// require the discord.js module
+const Discord = require('discord.js');
+// create a new Discord client
+const client = new Discord.Client();
+// Token Constant
+const { prefix, token } = require('./config.json');
+
+// when the client is ready, run this code
+// this event will only trigger one time after logging in
+client.once('ready', () => {
+	console.log('Ready!');
+});
+client.on('message', message => {
+	if (message.content === `${prefix}ping`) {
+		// send back "Pong." to the channel the message was sent in
+		message.channel.send('Pong.');
+	}
+});
+// login to Discord with your app's token
+client.login(token);
